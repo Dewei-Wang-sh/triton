cbb497a36480841fc30d1acb2c9990dfe3f18d57
Dewei Wang <Dewei.Wang@amd.com>
2025-12-26 02:38:59 -0600

    remove perphase; add fixup for 128x128x64; add dump log and rocprof

---
+++ python/tutorials/run_batch/test.matmul.padded.bash.py
@@ -0,0 +1,206 @@
+import torch
+
+import triton
+import triton.language as tl
+import argparse
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+
+def is_cuda():
+    return triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+
+
+
+
+@triton.jit
+def matmul_kernel(
+        # Pointers to matrices
+        a_ptr, b_ptr, c_ptr,
+        # Matrix dimensions
+        M, N, K,
+        # The stride variables represent how much to increase the ptr by when moving by 1
+        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+        # by to get the element one row down (A has M rows).
+        stride_am, stride_ak,  #
+        stride_bk, stride_bn,  #
+        stride_cm, stride_cn,
+        # Meta-parameters
+        BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,  #
+        GROUP_SIZE_M: tl.constexpr,  #
+        ACTIVATION: tl.constexpr  #
+):
+    """Kernel for computing the matmul C = A x B.
+    A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+    """
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    pid_m = pid // num_pid_n
+    pid_n = pid % num_pid_n
+
+    # -----------------------------------------------------------
+    # Add some integer bound assumptions.
+    # This helps to guide integer analysis in the backend to optimize
+    # load/store offset address calculation
+    tl.assume(pid_m >= 0)
+    tl.assume(pid_n >= 0)
+    tl.assume(stride_am > 0)
+    tl.assume(stride_ak > 0)
+    tl.assume(stride_bn > 0)
+    tl.assume(stride_bk > 0)
+    tl.assume(stride_cm > 0)
+    tl.assume(stride_cn > 0)
+
+    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+
+    cnt = tl.cdiv(K, BLOCK_SIZE_K)
+    tl.assume(cnt > 2)
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for k in range(0, cnt):
+        a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+        b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+        accumulator = tl.dot(a, b, accumulator)
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * stride_bk
+    if ACTIVATION == "leaky_relu":
+        accumulator = leaky_relu(accumulator)
+    c = accumulator.to(tl.float16)
+
+    # -----------------------------------------------------------
+    # Write back the block of the output matrix C with masks.
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, c, mask=c_mask)
+
+
+# We can fuse `leaky_relu` by providing it as an `ACTIVATION` meta-parameter in `matmul_kernel`.
+@triton.jit
+def leaky_relu(x):
+    return tl.where(x >= 0, x, 0.01 * x)
+
+
+
+def matmul(a, b, BM=128, BN=128, BK=64, nonKDim=32, activation=""):
+    # Check constraints.
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+    M, K = a.shape
+    K, N = b.shape
+    # Allocates output.
+    c = torch.empty((M, N), device=a.device, dtype=torch.float16)
+
+    # 1D launch kernel where each block gets its own program.
+    grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']), )
+    matmul_kernel[grid](
+        a, b, c,  #
+        M, N, K,  #
+        a.stride(0), a.stride(1),  #
+        b.stride(0), b.stride(1),  #
+        c.stride(0), c.stride(1),  #
+        ACTIVATION=activation, #
+        BLOCK_SIZE_M=BM,
+        BLOCK_SIZE_N=BN,
+        BLOCK_SIZE_K=BK,
+        GROUP_SIZE_M=1,
+        matrix_instr_nonkdim=nonKDim,
+        num_warps=4,
+        num_stages=2,
+        # for 256x256x32
+        #num_warps=8,
+        #num_stages=3,
+        kpack=1,
+    )
+    return c
+
+# Parse command line arguments for M, N, K
+parser = argparse.ArgumentParser(description='Triton Matrix Multiplication Benchmark')
+parser.add_argument('--BM', type=int, default=64, help='Matrix Block M (default: 64)')
+parser.add_argument('--BN', type=int, default=64, help='Matrix Block N (default: 64)')
+parser.add_argument('--BK', type=int, default=64, help='Matrix Block K (default: 64)')
+parser.add_argument('--nonKDim', type=int, default=16, help='Matrix MFMA nonKDim (default: 16)')
+args = parser.parse_args()
+
+bm = args.BM
+bn = args.BN
+bk = args.BK
+nonKDim = args.nonKDim
+
+print(f"Running benchmark with BM={bm}, BN={bn}, BK={bk}, MFMA_nonKDim{nonKDim}")
+
+
+# %%
+# Unit Test
+# ---------
+#
+# We can test our custom matrix multiplication operation against a native torch implementation (i.e., cuBLAS).
+
+torch.manual_seed(0)
+a = torch.rand((512, 512), device=DEVICE, dtype=torch.float16) - 0.5
+b = torch.rand((512, 512), device=DEVICE, dtype=torch.float16) - 0.5
+triton_output = matmul(a, b, bm, bn, bk, nonKDim)
+torch_output = torch.matmul(a, b)
+if torch.allclose(triton_output, torch_output, atol=1e-2, rtol=0):
+    print("✅ Triton and Torch match")
+else:
+    print("❌ Triton and Torch differ")
+
+
+
+ref_lib = 'cuBLAS' if is_cuda() else 'rocBLAS'
+
+configs = []
+#prof
+for BM in [bm]:
+    for BN in [bn]:
+        for BK in [bk]:
+            for nonKDim in [nonKDim]:
+                configs.append(
+                    triton.testing.Benchmark(
+                        x_names=["M", "N", "K"],  # Argument names to use as an x-axis for the plot
+                        x_vals=[4096],  # Different possible values for `x_name`
+                        line_arg="provider",  # Argument name whose value corresponds to a different line in the plot
+                        # Possible values for `line_arg`
+                        # Don't compare to cublas for fp8 cases as torch.matmul doesn't support fp8 at the moment.
+                        line_vals=["triton"],
+                        line_names=["Triton"],
+                        styles=[("green", "-"), ("blue", "-")],
+                        ylabel="TFLOPS",  # Label name for the y-axis
+                        plot_name=f"matmul-performance-BM{BM}-BN{BN}-BK{BK}-mfma{nonKDim}",
+                        args={
+                            "BM": BM,
+                            "BN": BN,
+                            "BK": BK,
+                            "nonKDim": nonKDim,
+                            #"GROUP_SIZE_M": 1,
+                            #num_warps: 4,
+                            #num_stages: 2,
+                        },
+                    ))
+
+
+@triton.testing.perf_report(configs)
+def benchmark(M, N, K, provider, BM, BN, BK, nonKDim):
+    a = torch.randn((M, K), device=DEVICE, dtype=torch.float16)
+    b = torch.randn((K, N), device=DEVICE, dtype=torch.float16)
+    quantiles = [0.5, 0.2, 0.8]
+    if provider == ref_lib.lower():
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch.matmul(a, b), quantiles=quantiles)
+    #extra_args = {"BLOCK_SIZE_M":BM, "BLOCK_SIZE_N": BN, "BLOCK_SIZE_K": BK, "GROUP_SIZE_M": 1, "matrix_instr_nonkdim":nonKDim}
+    #if provider == 'triton':
+    #    ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul(a, b, extra_args), quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul(a, b, BM, BN, BK, nonKDim), quantiles=quantiles)
+    perf = lambda ms: 2 * M * N * K * 1e-12 / (ms * 1e-3)
+    return perf(ms), perf(max_ms), perf(min_ms)
+
+
+benchmark.run(show_plots=True, print_data=True)
---
+++ python/tutorials/run_batch_new/run.padded.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Run to get log with different configurations
+# Usage: bash run.padded.log.sh
+
+echo "Start runs"
+
+export TRITON_HIP_USE_ASYNC_COPY=1
+
+## this is for collecting log
+#export MLIR_ENABLE_DUMP=1 TRITON_DISABLE_LINE_INFO=1 AMDGCN_ENABLE_DUMP=1
+
+#for BM in 128; do
+#    for BN in 128; do
+#        for BK in 64; do
+#            for nonKDim in 16 32; do
+for BM in 32 64 128; do
+    for BN in 128; do
+        for BK in 128; do
+            for nonKDim in 32; do
+                echo "Running: BM=${BM}, BN=${BN}, BK=${BK}, nonKDim=${nonKDim}"
+                #python test.matmul.padded.bash.py --BM ${BM} --BN ${BN} --BK ${BK} --nonKDim ${nonKDim} &> log.bm${BM}.bn${BN}.bk${BK}.nonkdim${nonKDim}.log
+                #echo "Completed: log.bm${BM}.bn${BN}.bk${BK}.nonkdim${nonKDim}.log"
+                # this is for collecting rocprof
+                rocprofv3 -i config.json -d att_bm${BM}_bn${BN}_bk${BK}_nonkdim${nonKDim} -- python test.matmul.padded.bash.py --BM ${BM} --BN ${BN} --BK ${BK} --nonKDim ${nonKDim}
+            done
+        done
+    done
+done
+
+echo "All runs completed!"
